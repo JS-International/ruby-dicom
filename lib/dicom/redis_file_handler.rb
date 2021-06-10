c66929d97ee6b04eee3a70bbde25869c8c505045
Richard dickson <richard@jumpshot.se>
2021-06-10 13:42:29 +0200

    Add redish filehandler to correct module

--- lib/dicom/redis_file_handler.rb
+++ lib/dicom/redis_file_handler.rb
@@ -1,12 +1,13 @@
-class RedisFileHandler < DICOM::FileHandler
-  require 'securerandom'
+module DICOM
+  class RedisFileHandler < DICOM::FileHandler
+    require 'securerandom'
 
 
-  def self.save_file(path_prefix, dcm, transfer_syntax)
-    # Save the DICOM object to Redis:
-    $redis.set(SecureRandom.uuid, dcm.to_hash.to_json)
-    message = [:info, "DICOM file processed "]
-    return message
+    def self.save_file(path_prefix, dcm, transfer_syntax)
+      # Save the DICOM object to Redis:
+      $redis.set(SecureRandom.uuid, dcm.to_hash.to_json)
+      message = [:info, "DICOM file processed "]
+      return message
+    end
   end
-  
 end
