b120baa7c225caaaa7a715a92365f3ca2b29bf0e
Michael Taylor <swedishpotato@gmail.com>
2021-09-18 15:03:15 +0200

    Add in AWS-SDK-SQS gem

--- lib/dicom/redis_file_handler.rb
+++ lib/dicom/redis_file_handler.rb
@@ -5,6 +5,7 @@ module DICOM
     def self.save_file(path_prefix, dcm, transfer_syntax)
       guid = SecureRandom.uuid
       # Save the DICOM object to Redis:
+      
       $redis.set(guid, dcm.to_hash.to_json)
       AWorker.perform_async(guid)
       message = [:info, "DICOM file processed with guid: #{guid}"]
